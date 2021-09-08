3afd49212ee21f67d8541b1ad404cb7c62d056fa
afecego <afecego@gmail.com>
2021-09-07 23:23:46 -0500

    Validation of lower or upper

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,5 @@
 def islower(c):
-    for i in range(65, 123):
-        if (ord(c) > 96 and ord(c) < 123):
-            return True
-        else:
-            return False
+    if (ord(c) > 96 and ord(c) < 123):
+        return True
+    else:
+        return False

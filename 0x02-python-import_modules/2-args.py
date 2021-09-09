fec71aa023d3ce8d000dc7772925cc8878fc905f
afecego <afecego@gmail.com>
2021-09-09 18:44:55 -0500

    Count of arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-from sys import argv
+if __name__ == '__main__':
+    from sys import argv
 
-
-def main():
     n = len(argv) - 1
     print('{:d} argument{}{}'.format(n, 's' if n == 0 or
-          n > 1 else '', '.' if n < 1 else ':'))
+                                     n > 1 else '', '.' if n < 1 else ':'))
     t = 0
     for i in argv:
         if t > 0:
             print('{:d}: {:s}'.format(t, i))
         t += 1
-
-if __name__ == '__main__':
-    main()

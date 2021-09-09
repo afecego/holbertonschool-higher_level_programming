f2abf9c052a70e604bef5fa5e145de7b3971a810
afecego <afecego@gmail.com>
2021-09-09 18:40:56 -0500

    Count of arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ from sys import argv
 def main():
     n = len(argv) - 1
     print('{:d} argument{}{}'.format(n, 's' if n == 0 or
-                                     n > 1 else '', '.' if n < 1 else ':'))
+          n > 1 else '', '.' if n < 1 else ':'))
     t = 0
     for i in argv:
         if t > 0:

1e991de01ed3eef487cf0cbde2160ba67ca3c719
afecego <afecego@gmail.com>
2021-09-13 22:36:48 -0500

    Replace letter

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string:
-        new = my_string.translate({ord("c"): None})
-        new = new.translate({ord("C"): None})
-        return new
+    new = my_string.translate({ord("c"): None})
+    new = new.translate({ord("C"): None})
+    return new

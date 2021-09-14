c41eb8a4bef8b58ee3797f9e8be79ac0a6581614
afecego <afecego@gmail.com>
2021-09-14 01:22:34 -0500

    Find multiplos of 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    list = []
-    for i in my_list:
-        if my_list[i] % 2 == 0:
-            list.append(1)
-        else:
-            list.append(0)
-    return list
+    if my_list:
+        list = []
+        for i in my_list:
+            if my_list[i] % 2 == 0:
+                list.append(1)
+            else:
+                list.append(0)
+        return list
+    else:
+        return None

123cdb54f892ed4501dd83ca4ae8e5254326818f
afecego <afecego@gmail.com>
2021-09-13 23:31:53 -0500

    Tupla in a sentence

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     if sentence is None:
         sentence[0] = None
-        tupla = len(sentence), sentence[0]
+        tupla = 0, sentence[0]
         return tupla
     else:
         tupla = len(sentence), sentence[0]

b4e9100703f42c9fde23bb483dfd2c3141f1f13f
afecego <afecego@gmail.com>
2021-09-15 22:08:20 -0500

    Delete a element in a dictionary

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,4 +3,4 @@ def multiply_by_2(a_dictionary):
     dictionary_2 = a_dictionary.copy()
     for i, j in dictionary_2.item():
         dictionary_2[i] = j * 2
-        return dictionary_2
+    return dictionary_2

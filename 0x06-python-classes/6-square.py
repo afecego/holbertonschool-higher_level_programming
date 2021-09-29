760a09a3092850c0c51397c00c7bb0aa3a469176
afecego <afecego@gmail.com>
2021-09-29 17:23:59 -0500

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -36,7 +36,7 @@ class Square:
         if (type(value) != tuple or len(value) != 2 or
             value[0] < 0 or value[1] < 0 or type(value[0]) is not int or
                 type(value[1]) is not int):
-            raise ValueError("size must be an integer")
+            raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
 

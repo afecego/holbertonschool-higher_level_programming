e4aab435524ef2979e7aa01843bc23ecfe6544b2
afecego <afecego@gmail.com>
2021-09-30 20:00:15 -0500

    Locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""Locked class"""
 
 
 class LockedClass(object):
+    """Locking class"""
     __slots__ = ['first_name']
 
     def __init__(self, v=0):
+        """initialization of class"""
         self.first_name = v

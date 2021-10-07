7bc1c618ca3e62f951243689326286deffc54e2a
afecego <afecego@gmail.com>
2021-10-07 16:59:12 -0500

    Correction test add

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -28,4 +28,8 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
     if (a + b) == float('inf') or (a + b) == -float('inf'):
         raise OverflowError("Number too large")
+    if a is None or (type(a) is not int and type(a) is not float):
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
     return a + b

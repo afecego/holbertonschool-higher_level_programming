3a92208852d3cff4bc19eb26bfa9c916c83f4a39
afecego <afecego@gmail.com>
2021-10-07 22:06:39 -0500

    Matrix multiply whit Numpy

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that multiplies 2 matrices using modulo Numpy"""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """Return matrix product"""
+    return(np.dot(m_a, m_b))

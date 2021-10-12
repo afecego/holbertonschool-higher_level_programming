d90150a3fb13b162f664c9dc7f069911cacc1274
afecego <afecego@gmail.com>
2021-10-11 23:47:47 -0500

    Method whit exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write a class BaseGeometry (based on 5-base_geometry.py)"""
+
+
+class BaseGeometry:
+    """Public instance methods"""
+    def area(self):
+        """return exception"""
+        raise Exception("area() is not implemented")

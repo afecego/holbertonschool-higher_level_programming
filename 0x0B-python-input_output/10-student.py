5cf388b94256989f14e57e02d6e2025aaff39cf3
afecego <afecego@gmail.com>
2021-10-13 00:04:28 -0500

    Info of studen in dictionary

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Write a class Student"""
+
+
+class Student():
+    """defines a student"""
+    first_name = ""
+    last_name = ""
+    age = 0
+
+    def __init__(self, first_name, last_name, age):
+        """Constructor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student"""
+        if type(attrs) is list:
+            if (type(line) is str for line in attrs):
+                eq = {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+                return eq
+        return self.__dict__

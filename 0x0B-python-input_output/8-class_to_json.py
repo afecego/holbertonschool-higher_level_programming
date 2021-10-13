a4cfc3417e155b40ac54fab167a24d655692b1a7
afecego <afecego@gmail.com>
2021-10-12 23:24:59 -0500

    Dictionary description

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 """function that returns the dictionary description
 with simple data structure"""
-import json
 
 
 def class_to_json(obj):
     """JSON serialization of an object"""
-    try:
-        return obj.toJSON()
-    except Exception as ex:
-        return obj.__dict__
+    return obj.__dict__

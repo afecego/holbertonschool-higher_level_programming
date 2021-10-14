79e9934f27ed57532602cb6c756be93e066e3451
afecego <afecego@gmail.com>
2021-10-13 23:43:13 -0500

    Pascal

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -5,11 +5,11 @@ representing the Pascal’s triangle"""
 
 def pascal_triangle(n):
     """representing the Pascal’s triangle"""
-    end = n - 1
-    pascal = [[1]]
-
     if n < 0:
         return []
+    
+    end = n - 1
+    pascal = [[1]]
 
     for i in range(end):
         fila = [1]

5d9d0ce01eda2b1e0a183d66864dc00d74ea9566
afecego <afecego@gmail.com>
2021-10-18 13:41:52 -0500

    Firts part of project

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,6 +16,7 @@ class Base():
             self.id = Base.__nb_objects
 
     def to_json_string(list_dictionaries):
+        """return the JSON string representation of list_dictionaries """
         if list_dictionaries is None or list_dictionaries == []:
             return []
         else:

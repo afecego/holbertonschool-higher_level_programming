bcb7d3e4db0be9918bced2261d833157f818976a
afecego <afecego@gmail.com>
2021-10-18 20:51:54 -0500

    Create

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -46,3 +46,13 @@ class Base():
         if json_string is None:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        if (cls.__name__ == "Square"):
+            dummy = cls(3)
+        if (cls.__name__ == "Rectangle"):
+            dummy = cls(3, 3)
+        dummy.update(**dictionary)
+        return dummy

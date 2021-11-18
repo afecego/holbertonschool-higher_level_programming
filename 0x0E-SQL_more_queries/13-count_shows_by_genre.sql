041e179fe7857a9ec90b54004f67b37e02ed8f66
afecego <afecego@gmail.com>
2021-11-17 20:51:20 -0800

    Alias

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,6 +3,6 @@
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id
-ORDER BY tv_show_genres.genre_id 
+ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_show_genres.genre_id 
 ORDER BY number_of_shows ASC;

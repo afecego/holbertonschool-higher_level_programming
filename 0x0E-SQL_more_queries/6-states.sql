4194810e0d6dc9a89a5860559ec860e09658d0a9
afecego <afecego@gmail.com>
2021-11-17 18:35:29 -0800

    New database

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- Write a script that creates the database hbtn_0d_usa and the table
 -- states (in the database hbtn_0d_usa) on your MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL).hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL).hbtn_0d_usa;
 

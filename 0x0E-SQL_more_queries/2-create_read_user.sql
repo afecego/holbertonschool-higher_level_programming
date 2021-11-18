b9fe55bc1c38ce792c2017f38916b33ab41bb63b
afecego <afecego@gmail.com>
2021-11-17 17:48:16 -0800

    create all

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,4 +3,5 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 CREATE USER IF NOT EXISTS user_0d_2@localhot IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhot;
+FLUSH PRIVILEGES;
 

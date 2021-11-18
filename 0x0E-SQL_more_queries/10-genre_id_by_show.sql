bf20af22a54105d611dbecc232fc5dfbc642ea3c
afecego <afecego@gmail.com>
2021-11-17 20:25:00 -0800

    Join on tv

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- Write a script that lists all shows contained in hbtn_0d_tvshows
+-- that have at least one genre linked.
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tvshows
+INNER JOIN tv_show_genres
+ON tv_shows.title = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC

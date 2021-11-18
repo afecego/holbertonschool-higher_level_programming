7584a8dd09bbfd2ad28db63ba41c79a3817cd446
afecego <afecego@gmail.com>
2021-11-17 20:30:08 -0800

    Join on tv

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,5 +3,5 @@
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tvshows
 INNER JOIN tv_show_genres
-ON tv_shows.title = tv_show_genres.genre_id
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;

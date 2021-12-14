edc509c176480109061fa87559857c51f1fc2966
afecego <afecego@gmail.com>
2021-12-13 20:59:54 -0500

    Print type number

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
-if (isNaN(process.argv[2])) {
-  console.log('Not a number');
+if (process.argv[2]) {
+  if (isNaN(process.argv[2])) {
+    console.log('Not a number');
+  } else {
+    console.log((process.argv[2]));
+  }
 } else {
-  console.log((process.argv[2]));
+  console.log('Not a number');
 }

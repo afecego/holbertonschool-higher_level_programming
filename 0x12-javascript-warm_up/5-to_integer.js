8f1c25f0606c02ba5ab2cf876928d078450cebec
afecego <afecego@gmail.com>
2021-12-13 21:01:44 -0500

    Print type number

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@ if (process.argv[2]) {
   if (isNaN(process.argv[2])) {
     console.log('Not a number');
   } else {
-    console.log((process.argv[2]));
+    console.log('My number is: ' + process.argv[2]);
   }
 } else {
   console.log('Not a number');

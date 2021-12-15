5b400c03c9e6c5cc3a3d9e7758937c6526731671
afecego <afecego@gmail.com>
2021-12-14 22:26:39 -0500

    Print rectangle whit a letter

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const Square1 = require('./5-square.js');
+class Square extends Square1 {
+  charPrint (c) {
+    if (c === undefined) {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          process.stdout.write('X');
+        }
+        console.log('');
+      }
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          process.stdout.write(c);
+        }
+        console.log('');
+      }
+    }
+  }
+}
+module.exports = Square;

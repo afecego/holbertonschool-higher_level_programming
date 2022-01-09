0f536cb18ff7b8aeaf39999bf9c776d00816dc7d
afecego <afecego@gmail.com>
2022-01-08 22:58:23 -0500

    Body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # takes in a URL, sends a request to that URL, and displays the size of the body
 
-curl -sI $1 | grep -i content-length | awk '{print $2}'
+curl -sI "$1" | grep -i content-length | awk '{print $2}'

2283ff54a248f92b3abfe54160a2c3be6e97f1cf
afecego <afecego@gmail.com>
2022-01-13 16:24:48 -0500

    Advance

---
+++ 0x11-python-network_1/100-github_commits.py.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Write a Python script that takes 2 arguments
+in order to solve this challenge
+"""
+from sys import argv
+import requests
+
+
+if __name__ == '__main__':
+    url = "https://api.github.com/user"
+    r = requests.get(url, auth=(argv[1], argv[2]))
+    res = r.json()
+    print(res.get('id'))

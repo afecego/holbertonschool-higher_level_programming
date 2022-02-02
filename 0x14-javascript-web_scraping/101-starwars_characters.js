1d1dbe6834d49b49dbe4efe5a7456b5a67dea289
afecego <afecego@gmail.com>
2022-02-02 00:16:08 -0500

    Advanced

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,12 +2,13 @@
 const request = require('request');
 const argv = process.argv.slice(2);
 const url = 'https://swapi-api.hbtn.io/api/films/' + argv[0];
+
 request(url, (err, res, body) => {
   if (err) {
     console.error(err);
   }
-    const characters = JSON.parse(body).characters;
-    helperChar(characters, 0);
+  const characters = JSON.parse(body).characters;
+  helperChar(characters, 0);
 });
 
 function helperChar (chars, i) {
